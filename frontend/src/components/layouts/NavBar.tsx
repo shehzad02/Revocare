3c2eeaba835c2215d3a7c130b6742a536c4b00be
jmho <justinho@ufl.edu>
2023-03-04 13:13:55 -0500

    begun navbar

---
+++ frontend/src/components/layouts/NavBar.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const NavBarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  height: 4rem;
+  background-color: #fff;
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+`;
