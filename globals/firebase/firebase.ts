dff980e8baf61136836aa2816f47ee1cf309c25c
Shehzad Shah <64668205+shehzad02@users.noreply.github.com>
2023-03-04 14:19:09 -0500

    Adds firebase beginnings

--- globals/firebase/firebase.ts
+++ globals/firebase/firebase.ts
@@ -2,8 +2,6 @@
 
 import { initializeApp } from "firebase/app";
 
-import { getAnalytics } from "firebase/analytics";
-
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
