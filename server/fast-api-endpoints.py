60fed41e06316927f4be5eacbd85b7f36da3f01a
benny <bennycortese@gmail.com>
2023-03-04 11:26:02 -0500

    Added mutexish lol

--- server/fast-api-endpoints.py
+++ server/fast-api-endpoints.py
@@ -8,6 +8,16 @@ stub = modal.Stub("zoom")
 
 web_image = modal.Image.debian_slim()
 
+cur_num = None
+
+
+def score_func():
+    num = 100
+    num *= 100
+    num /= 100
+    num *= 100
+    return num
+
 
 @web_app.post("/foo")
 async def foo(request: Request):
@@ -15,11 +25,31 @@ async def foo(request: Request):
     return body
 
 
+@web_app.get("/cur_score")
+async def cur_score():
+    return score_func()
+
+
+@web_app.get("/get_score")
+async def get_score():
+    toReturn = ""
+    if cur_num != None:
+        toReturn = cur_num
+        cur_num = None
+    return toReturn
+
+
+@web_app.post("/post_score")
+async def post_score(request: Request):
+    body = await request.json()
+    cur_num = body
+
+
 @web_app.get("/")
 async def bar(arg="world"):
     return HTMLResponse(f"<h1>Hello Fast {arg}!</h1>")
 
 
-@stub.asgi(image=web_image, gpu=modal.gpu.A100(memory=20))
+@stub.asgi(image=web_image)
 def fastapi_app():
     return web_app
