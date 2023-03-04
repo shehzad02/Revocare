383ff36ae83997cd3ecc9088941c560ab4076d5d
benny <bennycortese@gmail.com>
2023-03-03 21:05:10 -0500

    Modal serverless insta deploys

--- fast-api-endpoints.py
+++ fast-api-endpoints.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+import modal
+
+web_app = FastAPI()
+stub = modal.Stub("zoom")
+
+web_image = modal.Image.debian_slim()
 
 
-@app.get("/")
-def home_page():
-    return "Hi"
+@web_app.post("/foo")
+async def foo(request: Request):
+    body = await request.json()
+    return body
+
+
+@web_app.get("/")
+async def bar(arg="world"):
+    return HTMLResponse(f"<h1>Hello Fast {arg}!</h1>")
+
+
+@stub.asgi(image=web_image)
+def fastapi_app():
+    return web_app
+
+
+stub.run()
